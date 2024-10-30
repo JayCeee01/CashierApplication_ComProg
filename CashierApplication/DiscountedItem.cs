907cc8366ffd52696395a10bb62d0cbf9a267dcd
JayCee <158656310+JayCeee01@users.noreply.github.com>
2024-10-30 19:27:05 +0800

    Add project files.

---
+++ CashierApplication/DiscountedItem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ItemNamespace
+{
+    internal class DiscountedItem : Item
+    {
+        private double item_discount, discounted_price, payment_amount, change;
+
+        public DiscountedItem(string name, double price, int quantity, double discount) : base(name, price, quantity)
+        {
+            this.item_discount = discount;
+        }
+
+        public override double getTotalPrice()
+        {
+            double discountItem = item_discount * 0.01;
+            double result = discountItem * item_price;
+            this.discounted_price = (item_price - result) * item_quantity;
+            double finalDiscount = this.discounted_price;
+            return finalDiscount;
+        }
+
+        public override void setPayment(double amount)
+        {
+
+            if (payment_amount < 0)
+            {
+                throw new Exception("Please enter a valid payment amount.");
+            }
+            else
+            {
+                this.payment_amount = amount;
+            } 
+        }
+
+        public double getChange()
+        {
+
+            if (this.discounted_price == 0)
+            {
+                getTotalPrice(); 
+            }
+            this.change = this.payment_amount - this.discounted_price;
+
+            if (this.change < 0)
+            {
+                MessageBox.Show("Please enter a valid payment amount.");
+                return 0.00;
+            }
+            else
+            {
+             return this.change;
+
+            }  
+        }
+    }
+}
---
+++ CashierApplication/frmPurchaseDiscountedItem.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using ItemNamespace;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashierApplication
+{
+    public partial class frmPurchaseDiscountedItem : Form
+    {
+        public frmPurchaseDiscountedItem()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string itemName = textBox1.Text;
+                int discountItem = int.Parse(textBox2.Text);
+                double price = double.Parse(textBox5.Text);
+                int quantityItem = int.Parse(textBox3.Text);
+
+                if (itemName == "" || discountItem <0 || discountItem > 100 || price <0 || quantityItem < 0)
+                {
+                    MessageBox.Show("An error has occured! Please enter the valid input on each box.");
+                } else
+                {
+                    DiscountedItem d = new DiscountedItem(itemName, price, quantityItem, discountItem);
+
+                    label6.Text = $"$ {Convert.ToString(d.getTotalPrice())}";
+                }
+ 
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("An error has occured! Please enter the valid input on each box.");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string itemName = textBox1.Text;
+                int discountItem = int.Parse(textBox2.Text);
+                double price = double.Parse(textBox5.Text);
+                int quantityItem = int.Parse(textBox3.Text);
+                double payment = double.Parse(textBox4.Text);
+
+                DiscountedItem d2 = new DiscountedItem(itemName, price, quantityItem, discountItem);
+                d2.setPayment(payment);
+
+                label4.Text = $"$ {Convert.ToString(d2.getChange())}";
+            }
+            catch (Exception)
+            {
+                textBox4.Text = "";
+                MessageBox.Show("An error has occured! Please enter the valid input on each box.");
+            }
+        }
+    }
+}
